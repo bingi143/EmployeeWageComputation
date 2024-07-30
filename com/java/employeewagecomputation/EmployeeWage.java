0821564ee856c1ab9b7a99519912af07880f2326
bingi143 <bingivenkateswarlu578@gmail.com>
2024-07-30 15:52:11 +0530

    Added part Time employee wage functionality

--- com/java/employeewagecomputation/EmployeeWage.java
+++ com/java/employeewagecomputation/EmployeeWage.java
@@ -3,26 +3,37 @@ package com.java.employeewagecomputation;
 public class EmployeeWage {
 	public static void main(String []args) {
 		System.out.println("Welcome to Employee Wage ComputationProgram");
-        int wagePerHour = 20;
-        int fullDay = 8;
-        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
-        System.out.println("");
+		 int wagePerHour = 20;
+	        int fullDay = 8;
+	        int halfDay = 4;
+	        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+	        System.out.println("");
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if(empCheck == 0)
-        {
-            System.out.print("Employee is Present");
-            System.out.println("");
-            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
-            System.out.println("");
-        }
-        else
-        {
-            System.out.print("Employee is Absent");
-            System.out.println("");
-            System.out.println("The daily wage of Employee is : 0 " );
-            System.out.println("");
-        }
+	        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+	        if( empCheck == 0 )
+	        {
+	            System.out.print("Employee is Present");
+	            System.out.println("");
+	            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+	            System.out.println("");
+	        }
+
+	        else if ( empCheck == 1 )
+
+	        {
+	            System.out.print("Employee is Present but working part time ");
+	            System.out.println("");
+	            System.out.print("The daily wage of Employee is : " + ( wagePerHour * halfDay ));
+	            System.out.println("");
+	        }
+	        else
+	        {
+	            System.out.print("Employee is Absent");
+	            System.out.println("");
+	            System.out.println("The daily wage of Employee is : 0 " );
+	            System.out.println("");
+	        }
+        
 	
 	}
 
