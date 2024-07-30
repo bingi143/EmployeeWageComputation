4adda998780e984ebe0f1b9f6c78cfd7014f7875
bingi143 <bingivenkateswarlu578@gmail.com>
2024-07-30 15:28:09 +0530

    Added functionality to check employee attendance

--- com/java/employeewagecomputation/EmployeeWage.java
+++ com/java/employeewagecomputation/EmployeeWage.java
@@ -2,6 +2,17 @@ package com.java.employeewagecomputation;
 
 public class EmployeeWage {
 	public static void main(String []args) {
-		System.out.println("Welcome to Employee Wage ComputationProgram");	}
+		System.out.println("Welcome to Employee Wage ComputationProgram");
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == 0)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+	
+	}
 
 }
