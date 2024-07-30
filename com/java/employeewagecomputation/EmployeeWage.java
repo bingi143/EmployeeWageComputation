8fc3f19c26c9668f1f43e983368dc9bbd67f6ca6
bingi143 <bingivenkateswarlu578@gmail.com>
2024-07-30 15:18:13 +0530

    Modified welcome message

--- com/java/employeewagecomputation/EmployeeWage.java
+++ com/java/employeewagecomputation/EmployeeWage.java
@@ -1,5 +1,7 @@
 package com.java.employeewagecomputation;
 
 public class EmployeeWage {
+	public static void main(String []args) {
+		System.out.println("Welcome to Employee Wage ComputationProgram");	}
 
 }
