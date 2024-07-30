802ffa4e2e32784e80f2b8f7adb0762cfcf914bb
bingi143 <bingivenkateswarlu578@gmail.com>
2024-07-30 16:11:11 +0530

    Using Swich case Emp Wage

--- com/java/employeewagecomputation/EmployeeWage.java
+++ com/java/employeewagecomputation/EmployeeWage.java
@@ -10,30 +10,30 @@ public class EmployeeWage {
 	        System.out.println("");
 
 	        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	        if( empCheck == 0 )
-	        {
-	            System.out.print("Employee is Present");
-	            System.out.println("");
-	            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
-	            System.out.println("");
+	        switch ( empCheck ) {
+	            case 0:
+	                System.out.print("Employee is Present");
+	                System.out.println("");
+	                System.out.print("The daily wage of Employee is : " + (wagePerHour * fullDay));
+	                System.out.println("");
+	                break;
+
+	            case 1:
+	                System.out.print("Employee is Present but working part time ");
+	                System.out.println("");
+	                System.out.print("The daily wage of Employee is : " + (wagePerHour * halfDay));
+	                System.out.println("");
+	                break;
+
+	            case 2:
+	                System.out.print("Employee is Absent");
+	                System.out.println("");
+	                System.out.println("The daily wage of Employee is : 0 ");
+	                System.out.println("");
+	                break;
+
 	        }
 
-	        else if ( empCheck == 1 )
-
-	        {
-	            System.out.print("Employee is Present but working part time ");
-	            System.out.println("");
-	            System.out.print("The daily wage of Employee is : " + ( wagePerHour * halfDay ));
-	            System.out.println("");
-	        }
-	        else
-	        {
-	            System.out.print("Employee is Absent");
-	            System.out.println("");
-	            System.out.println("The daily wage of Employee is : 0 " );
-	            System.out.println("");
-	        }
-        
 	
 	}
 
